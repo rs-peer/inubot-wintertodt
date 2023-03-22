12200524ad0b298ca290badafeece2cf8d0f9a8e
root <parny_fan2003@doglover.com>
2023-03-22 12:43:39 +0000

    small fix

--- src/main/java/org/rspeer/scripts/wintertodt/task/game/WaitingAreaTask.java
+++ src/main/java/org/rspeer/scripts/wintertodt/task/game/WaitingAreaTask.java
@@ -2,8 +2,8 @@ package org.rspeer.scripts.wintertodt.task.game;
 
 import com.google.inject.Inject;
 import org.rspeer.game.adapter.component.inventory.Inventory;
-import org.rspeer.game.adapter.scene.Player;
 import org.rspeer.game.component.Item;
+import org.rspeer.game.effect.Health;
 import org.rspeer.game.movement.Movement;
 import org.rspeer.game.scene.Players;
 import org.rspeer.game.script.TaskDescriptor;
@@ -17,8 +17,6 @@ import org.rspeer.scripts.wintertodt.domain.Domain;
 )
 public class WaitingAreaTask extends GameTask {
 
-  private int tolerance = Constant.EAT_FOOD_AT.minimum();
-
   @Inject
   public WaitingAreaTask(Domain domain) {
     super(domain);
@@ -26,16 +24,18 @@ public class WaitingAreaTask extends GameTask {
 
   @Override
   protected boolean play() {
-    if (domain.getBoss().isRespawning()) {
+    if (domain.getBoss().isRespawning() || Health.getPercent() > 30) {
       return false;
     }
+
+    //TODO if enough time and points are low just reset and bank...
 
     Item food = Inventory.backpack().query().actions("Eat", "Drink").results().first();
     if (food != null) {
       return false;
     }
 
-    if (Constant.WAITING_AREA.contains(Players.self()) ) {
+    if (Constant.WAITING_AREA.contains(Players.self())) {
       return true;
     }
 
