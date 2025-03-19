faf76152da4af2bce995673e36c5597de02c3be0
root <parny_fan2003@doglover.com>
2025-03-19 15:29:01 +0000

    this is how it works i think

--- src/main/java/org/rspeer/scripts/wintertodt/api/Warmth.java
+++ src/main/java/org/rspeer/scripts/wintertodt/api/Warmth.java
@@ -5,6 +5,11 @@ import org.rspeer.game.Vars;
 public class Warmth {
 
   public static int getPercent() {
-    return Vars.get(Vars.Type.VARBIT, 11434);
+    int warmth = Vars.get(Vars.Type.VARBIT, 11434);
+    if (warmth == 0) {
+      return 0;
+    }
+
+    return warmth / 10;
   }
 }
